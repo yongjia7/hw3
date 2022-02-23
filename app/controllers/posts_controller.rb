222f2bfc269831b10826031e7f0b870b47b33cd8
yongjia7 <yongjia.zhang@kellogg.northwestern.edu>
2022-02-23 07:12:47 +0000

    final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+
+    def new
+        @post = Post.new
+        @post.place_id = params["place_id"]
+    end
+
+    def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places"
+    end
 end
